6cff2022238f48847ee36beb62300feb1e91f399
YoungDHi <sm930219@naver.com>
2023-01-06 10:42:07 +0900

    ex

--- src/com/iu/api/strings/StringEx2.java
+++ src/com/iu/api/strings/StringEx2.java
@@ -5,28 +5,33 @@ import java.util.Scanner;
 public class StringEx2 {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		Scanner sc = new Scanner(System.in);
 		String [] names = {"jpg", "jpeg", "gif", "png"};
 		System.out.println("파일명을 입력");
-		String filename = sc.next();//sf.sd.dsf.df
+		String filename = sc.next();//sf.sd.jpg
 		
-		String result = filename.substring(filename.lastIndexOf(".")+1);
-		
-		
-		for(int i = 0;i<4;i++) {
-			boolean check = false;
-			if(result.equals(names [i])) {
+		//확장자 분리 작업
+		int idx = filename.lastIndexOf(".");
+		String result = filename.substring(idx+1);
+		boolean flag = true;
+		//확장자가 이미지인지 검증
+		for(int i=0;i<names.length;i++) {
+			if(result.equals(names[i])) {
 				System.out.println("정상적인 파일입니다");
-				check = true;
+				//flag = false;
+				flag=!flag;
 				break;
-			}	
-		if(check) {
-			System.out.println("비정상적인 파일입니다");
+			}
+		}
+		if(flag) {
+			System.out.println("비정상적인 파일입니다.");
 		}
 		
 		
 		
-		}
+		System.out.println("Finish");
+		
 	}
 
 }
