6ed975904a4784d6cb072e0c49ad915e3f620910
YoungDHi <sm930219@naver.com>
2023-01-09 16:14:45 +0900

    dd

--- src/com/iu/api/strings/ex3/WeatherData.java
+++ src/com/iu/api/strings/ex3/WeatherData.java
@@ -1,5 +1,7 @@
 package com.iu.api.strings.ex3;
 
+import java.util.StringTokenizer;
+
 public class WeatherData {
 	
 	private String data;
@@ -12,41 +14,60 @@ public class WeatherData {
 	//init 
 	//data를 파싱해서 각 data를 DTO에 대입하고 DTO들을 리턴
 	public WeatherDTO [] init() {
-		String [] datas = this.data.split(",");
+		StringTokenizer st = new StringTokenizer(this.data,",");
+		WeatherDTO [] Weathers = new WeatherDTO[st.countTokens()/4];
+		int i=0;
+		while(st.hasMoreTokens()) {
+			WeatherDTO weatherDTO = new WeatherDTO();
+			weatherDTO.setCity(st.nextToken()); 
+			weatherDTO.setGion(Integer.parseInt(st.nextToken()));
+			weatherDTO.setStatus(st.nextToken());
+			weatherDTO.setMise(Integer.parseInt(st.nextToken()));
+			Weathers[i]=weatherDTO;
+			i++;
+			
+			}
+		return Weathers;
 		
-		WeatherDTO [] weathers = new WeatherDTO[datas.length/4];
 		
 		
-		
+	}
+//	public WeatherDTO [] init() {
+//		String [] datas = this.data.split(",");
+//		
+//		WeatherDTO [] weathers = new WeatherDTO[datas.length/4];
+//		
+//		
+//		
+////		for(int i=0;i<datas.length;i++) {
+////			WeatherDTO weatherDTO = new WeatherDTO();
+////			for(int r=0;r<i+1;r++) {
+////				if(i==4*r) {
+////					weathers[i] = weatherDTO;
+////				} else if(i==4*r+1) {
+////					weathers[i].setGion(datas[i]);
+////				}else if(i==4*r+2) {
+////					weathers[i].setStatus(datas[i]);
+////				}else if(i==4*r+3) {
+////					weathers[i].setMise(datas[i]);
+////				}
+////			}
+////		}
+//		//int idx = 0;
 //		for(int i=0;i<datas.length;i++) {
 //			WeatherDTO weatherDTO = new WeatherDTO();
-//			for(int r=0;r<i+1;r++) {
-//				if(i==4*r) {
-//					weathers[i] = weatherDTO;
-//				} else if(i==4*r+1) {
-//					weathers[i].setGion(datas[i]);
-//				}else if(i==4*r+2) {
-//					weathers[i].setStatus(datas[i]);
-//				}else if(i==4*r+3) {
-//					weathers[i].setMise(datas[i]);
-//				}
-//			}
+//			weatherDTO.setCity(datas[i]);
+//			weatherDTO.setGion(Integer.parseInt(datas[++i]));
+//			weatherDTO.setStatus(datas[++i]);
+//			weatherDTO.setMise(Integer.parseInt(datas[++i]));
+//			weathers[i/4] = weatherDTO;
+//			//weathers[idx]=weatherDTO;
+//			//idx++;
 //		}
-		//int idx = 0;
-		for(int i=0;i<datas.length;i++) {
-			WeatherDTO weatherDTO = new WeatherDTO();
-			weatherDTO.setCity(datas[i]);
-			weatherDTO.setGion(Integer.parseInt(datas[++i]));
-			weatherDTO.setStatus(datas[++i]);
-			weatherDTO.setMise(Integer.parseInt(datas[++i]));
-			weathers[i/4] = weatherDTO;
-			//weathers[idx]=weatherDTO;
-			//idx++;
-		}
-		
-		
-		return weathers;
-	}
+//		
+//		
+//		return weathers;
+//	}
 	
 
 }
